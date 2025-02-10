4c71b97cd88fa290ecdfd1d84eadfe8460529eb7
Graham Gear <graham.gear@nowhere.org>
2025-02-10 14:45:47 +0800

    HACK-GRAHAM: format source

--- homeassistant/components/influxdb/__init__.py
+++ homeassistant/components/influxdb/__init__.py
@@ -10,10 +10,8 @@ import math
 import queue
 import threading
 import time
-import string
-import dateutil.parser
-
 from typing import Any
+
 from influxdb import InfluxDBClient, exceptions
 from influxdb_client import InfluxDBClient as InfluxDBClientV2
 from influxdb_client.client.write_api import ASYNCHRONOUS, SYNCHRONOUS
@@ -285,7 +283,8 @@ def _generate_event_to_json(conf: dict) -> Callable[[Event], dict[str, Any] | No
         ignore_attributes = set(entity_config.get(CONF_IGNORE_ATTRIBUTES, []))
         ignore_attributes.update(global_ignore_attributes)
 
-        # Build a unique key (recursively add '_' until unique) with optional type suffix
+        # HACK-GRAHAM-Start: Build a unique key (recursively add '_' until unique) with optional type suffix
+        import dateutil.parser
         def format_key(_key, _suffix=None):
             _suffix = "" if _suffix is None else f"_{_suffix}"
             key_suffix = f"{_key}{_suffix}".lower().replace(' ', '_') \
@@ -348,6 +347,7 @@ def _generate_event_to_json(conf: dict) -> Callable[[Event], dict[str, Any] | No
                 if key_root not in json[INFLUX_CONF_FIELDS] and key in json[INFLUX_CONF_FIELDS]:
                     json[INFLUX_CONF_FIELDS][key_root] = json[INFLUX_CONF_FIELDS][key]
                     del json[INFLUX_CONF_FIELDS][key]
+        # HACK-GRAHAM-Finish
 
         json[INFLUX_CONF_TAGS].update(tags)
 
